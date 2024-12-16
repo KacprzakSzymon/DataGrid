ae5b8f891e767485d81e59e00f123a7fd8386e66
Szymon Kacprzak <skacprzak@s201.local>
2024-12-16 08:04:35 +0100

    base app done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace DataGrid
 {
@@ -20,9 +21,21 @@ namespace DataGrid
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Produkt> ListaProduktow = null;
         public MainWindow()
         {
             InitializeComponent();
+            PrzygotujWiazanie();
+        }
+        private void PrzygotujWiazanie()
+        {
+            ListaProduktow = new ObservableCollection<Produkt>();
+            ListaProduktow.Add(new Produkt("01-11", "ołówek", 8, "Katowice1"));
+            ListaProduktow.Add(new Produkt("PW-20", "pióro wieczne", 75, "Katowice 2"));
+            ListaProduktow.Add(new Produkt("DZ-10", "długopis żelowy", 1121, "Katowice 1"));
+            ListaProduktow.Add(new Produkt("DZ-12", "długopis kulkowy", 280, "Katowice 2"));
+            gridProdukty.ItemsSource= ListaProduktow;
         }
     }
+    
 }
---
+++ Produkt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace DataGrid
+{
+    class Produkt
+    {
+        public Produkt() { }
+        public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public int LiczbaSztuk { get; set; }
+        public string Magazyn { get; set; }
+        public Produkt(string sym, string naz, int szt, string mag)
+        {
+            Symbol = sym;
+            Nazwa = naz;
+            LiczbaSztuk = szt;
+            Magazyn = mag;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3}", Symbol, Nazwa, LiczbaSztuk, Magazyn);
+        }
+        
+    }
+}
